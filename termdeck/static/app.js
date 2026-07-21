21bab62667617a671a6b7bb868a78a43d7c8066f
Dan <danial.farid@gmail.com>
2026-07-20 22:07:43 -0400

    Simplify processing indicator to a sliding bar

--- termdeck/static/app.js
+++ termdeck/static/app.js
@@ -565,7 +565,7 @@ class TermdeckApp {
       this.sessionStatusEls.set(s.session_id, dot);
       const spinner = document.createElement("span");
       spinner.className = "session-spinner";
-      spinner.innerHTML = "<i></i><i></i><i></i>";
+      spinner.innerHTML = "<i></i>";
       const presentation = this.titlePresentation(s);
       spinner.classList.toggle("on", presentation.spinning);
       this.sessionSpinnerEls.set(s.session_id, spinner);
