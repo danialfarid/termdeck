5fa0bb4b0df14a7306b5d054147c6e946479d84e
Dan <danial.farid@gmail.com>
2026-07-20 21:55:07 -0400

    Set spinner animation to half second

--- termdeck/static/app.js
+++ termdeck/static/app.js
@@ -1,6 +1,6 @@
 const REFRESH_MS = 5000;
 const TITLE_REFRESH_MS = 1000;
-const TITLE_SPINNER_MS = 1000;
+const TITLE_SPINNER_MS = 500;
 const CODEX_SPINNER_FRAMES = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"];
 const RECONNECT_MS = 1500;
 const DEFAULT_COMMAND = "codex";
